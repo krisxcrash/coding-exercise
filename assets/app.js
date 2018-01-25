ac013869a4757ca4dc9fd15a63270016fee62a25
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-24 21:56:54 -0800

    updated to test fetch function

--- assets/app.js
+++ assets/app.js
@@ -1,61 +1,67 @@
-const queryURL = 'https://jsonplaceholder.typicode.com';
+// const queryURL = 'https://jsonplaceholder.typicode.com';
 
-let posts = [];
-let post = 0;
-let numberOfPosts = 0;
-let numberOfAlbums = 0;
-let numberOfToDos = 0;
+// let posts = [];
+// let post = 0;
+// let numberOfPosts = 0;
+// let numberOfAlbums = 0;
+// let numberOfToDos = 0;
 
-const runPostQuery = (userId, postType, queryURL, numberOfPosts) => {
-    $.ajax({
-        url: queryURL + '/' + postType + '?userId=' + userId,
-        method: 'GET'
-      }).then(function(data) {
+// const runPostQuery = (userId, postType, queryURL, numberOfPosts) => {
+//     $.ajax({
+//         url: queryURL + '/' + postType + '?userId=' + userId,
+//         method: 'GET'
+//       }).then(function(data) {
         
-        for (let i = 0; i < 5; i++) {
-            const postData = data[i];
-            numberOfPosts++;
-            console.log(`Post ${numberOfPosts}: ${postData.title}`);
-        }
-    });
-}
+//         for (let i = 0; i < 5; i++) {
+//             const postData = data[i];
+//             numberOfPosts++;
+//             console.log(`Post ${numberOfPosts}: ${postData.title}`);
+//         }
+//     });
+// }
 
-const runAlbumQuery = (userId, postType, queryURL, numberOfAlbums) => {
-    $.ajax({
-        url: queryURL + '/' + postType + '?userId=' + userId,
-        method: 'GET'
-      }).then(function(data) {
-        console.log(data);
-        numberOfAlbums = data.length;
-    });
-}
+// const runAlbumQuery = (userId, postType, queryURL, numberOfAlbums) => {
+//     $.ajax({
+//         url: queryURL + '/' + postType + '?userId=' + userId,
+//         method: 'GET'
+//       }).then(function(data) {
+//         numberOfAlbums = data.length;
+//     });
+// }
 
-const runToDoQuery = (userId, postType, queryURL, numberOfAlbums) => {
-    $.ajax({
-        url: queryURL + '/' + postType + '?userId=' + userId,
-        method: 'GET'
-      }).then(function(data) {
-        console.log(data);
-        numberOfAlbums = data.length;
-        // console.log(numberOfAlbums);
-    });
-}
+// const runToDoQuery = (userId, postType, queryURL, numberOfAlbums) => {
+//     $.ajax({
+//         url: queryURL + '/' + postType + '?userId=' + userId,
+//         method: 'GET'
+//       }).then(function(data) {
+//         numberOfAlbums = data.length;
+//     });
+// }
 
-const getComments = (userId, postType, queryURL) => {
-    $.ajax({
-        url: queryURL + '/posts/1/' + postType + '?PostId=5',
-        method: 'GET'
-      }).then(function(data) {
-        console.log(`Viewing post ${'test'} which has ${data.length} comments`)
-    });
-}
+// const getComments = (userId, postType, queryURL) => {
+//     $.ajax({
+//         url: queryURL + '/posts/1/' + postType + '?PostId=5',
+//         method: 'GET'
+//       }).then(function(data) {
+//           console.log(data)
+//         console.log(`Viewing post ${'5'} which has ${data.length} comments`)
+//     });
+// }
 
-runPostQuery(1, 'posts', queryURL, numberOfPosts);
+// runPostQuery(1, 'posts', queryURL, numberOfPosts);
 
-runAlbumQuery(1, 'albums', queryURL, numberOfAlbums);
+// runAlbumQuery(1, 'albums', queryURL, numberOfAlbums);
 
-runToDoQuery(1, 'todos', queryURL, numberOfToDos);
+// runToDoQuery(1, 'todos', queryURL, numberOfToDos);
 
-getComments(1, 'comments', queryURL)
+// getComments(1, 'comments', queryURL)
 
-console.log(`Bret has ${numberOfPosts} posts, ${numberOfAlbums} albums, and ${numberOfToDos} todos`)
+// console.log(`Bret has ${numberOfPosts} posts, ${numberOfAlbums} albums, and ${numberOfToDos} todos`)
+
+const url = 'https://jsonplaceholder.typicode.com' + '/posts/1/comments?PostId=5';
+
+fetch(url)
+  .then((resp) => resp.json()) // Transform the data into json
+  .then(function(data) {
+    console.log(data);
+});
