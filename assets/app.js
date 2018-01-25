40a708e6ba460a375fe6cb056ee12ae984c57efc
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-24 20:28:05 -0800

    updated to add functions for each query

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,48 @@
-var root = 'https://jsonplaceholder.typicode.com';
+const queryURL = 'https://jsonplaceholder.typicode.com';
 
-$.ajax({
-  url: root + '/posts',
-  method: 'GET'
-}).then(function(data) {
-  console.log(data);
+let posts = [];
+let post = 0;
+let numberOfPosts = 0;
+let numberOfAlbums = 0;
+let numberOfToDos = 0;
 
-  for (let i = 0; i < 5; i++) {
-      const element = data[i];
-      console.log(element);
-      
-  }
+const runPostQuery = (userId, postType, queryURL, numberOfPosts) => {
+    $.ajax({
+        url: queryURL + '/' + postType + '?userId=' + userId,
+        method: 'GET'
+      }).then(function(data) {
+        console.log(data);
+        numberOfPosts = data.length;
 
+    });
+}
 
-});
+const runAlbumQuery = (userId, postType, queryURL, numberOfAlbums) => {
+    $.ajax({
+        url: queryURL + '/' + postType + '?userId=' + userId,
+        method: 'GET'
+      }).then(function(data) {
+        console.log(data);
+        numberOfAlbums = data.length;
+        // console.log(numberOfAlbums);
+    });
+}
+
+const runToDoQuery = (userId, postType, queryURL, numberOfAlbums) => {
+    $.ajax({
+        url: queryURL + '/' + postType + '?userId=' + userId,
+        method: 'GET'
+      }).then(function(data) {
+        console.log(data);
+        numberOfAlbums = data.length;
+        // console.log(numberOfAlbums);
+    });
+}
+
+runPostQuery(1, 'posts', queryURL, numberOfPosts);
+
+runAlbumQuery(1, 'albums', queryURL, numberOfAlbums);
+
+runToDoQuery(1, 'todos', queryURL, numberOfToDos)
+
+console.log(`Bret has ${numberOfPosts} posts, ${numberOfAlbums} albums, and ${numberOfToDos} todos`)
